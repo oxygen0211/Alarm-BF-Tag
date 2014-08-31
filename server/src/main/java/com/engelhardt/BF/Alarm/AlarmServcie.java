c2b2ee8f475600eee5eb805707531d95523a92e8
Julian Engelhardt <julian.engelhardt@gmx.de>
2014-08-31 22:07:02 +0200

    Correct alarm time calculation

--- server/src/main/java/com/engelhardt/BF/Alarm/AlarmServcie.java
+++ server/src/main/java/com/engelhardt/BF/Alarm/AlarmServcie.java
@@ -1,16 +1,16 @@
 package com.engelhardt.BF.Alarm;
 
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("{delay}")
 public class AlarmServcie {
 
 	@GET()
 	@Produces("text/plain")
-	public String triggerAlarm(@PathParam("delay") int delay) {
+	public String triggerAlarm(@PathParam("delay") int delay, @PathParam("group") String group) {
 		try {
 			AlarmManager.getInstance().addAlarm(delay);
 			return "ok";
